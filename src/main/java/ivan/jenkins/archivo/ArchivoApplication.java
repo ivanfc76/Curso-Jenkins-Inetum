8694361ba5f6ad3f48bab8decb46108cce7c03e1
ivanfc76 <ivanf76@gmail.com>
2024-10-02 11:33:29 +0200

    Add files via upload

---
+++ src/main/java/ivan/jenkins/archivo/ArchivoApplication.java
@@ -0,0 +1,13 @@
+package ivan.jenkins.archivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ivan/jenkins/archivo/ArchivoApplicationTests.java
@@ -0,0 +1,13 @@
+package ivan.jenkins.archivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
